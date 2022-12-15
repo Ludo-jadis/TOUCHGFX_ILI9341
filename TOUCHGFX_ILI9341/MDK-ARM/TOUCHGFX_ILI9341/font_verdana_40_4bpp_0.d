0c9dc247110790923b4d3ce3e588bf16291cd32c
Ludo-jadis <120654033+Ludo-jadis@users.noreply.github.com>
2022-12-15 14:54:20 +0100

    TOUCHGFX_ILI9341

---
+++ TOUCHGFX_ILI9341/MDK-ARM/TOUCHGFX_ILI9341/font_verdana_40_4bpp_0.d
@@ -0,0 +1,8 @@
+touchgfx_ili9341/font_verdana_40_4bpp_0.o: \
+  ..\touchgfx\generated\fonts\src\font_verdana_40_4bpp_0.cpp \
+  ..\middlewares\st\touchgfx\framework\include\touchgfx\hal\Types.hpp \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__config \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\middlewares\st\touchgfx\framework\include\touchgfx\hal\Config.hpp
---
+++ TOUCHGFX_ILI9341/MDK-ARM/TOUCHGFX_ILI9341/image_blue_progressindicators_fill_medium_circle_indicator_fill_full.d
@@ -0,0 +1,3 @@
+touchgfx_ili9341/image_blue_progressindicators_fill_medium_circle_indicator_fill_full.o: \
+  ..\touchgfx\generated\images\src\__designer\image_blue_progressindicators_fill_medium_circle_indicator_fill_full.cpp \
+  ..\middlewares\st\touchgfx\framework\include\touchgfx\hal\Config.hpp
